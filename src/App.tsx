9cfd3ce5d2c35c07a8845ae7f0e45f2a8483d381
Paul Amago <104170582+BowlofRocks@users.noreply.github.com>
2024-06-20 08:28:47 -0600

    started creating pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import NavBar from "./components/Nav";
+import AboutMe from "./components/Bio";
+import ContactMe from "./components/ContactMe";
 function App() {
-  return <header><NavBar/></header>
+  return (<>
+  <header><NavBar/></header>
+  <main>
+    <AboutMe/>
+    <ContactMe/>
+  </main>
+  </>);
 }
 
 export default App;
---
+++ src/components/Bio.tsx
@@ -0,0 +1,24 @@
+function AboutMe() {
+    return (<>
+    <div className="container ">
+  <div className="row">
+    <div className="col-lg-8 pt-4 pt-lg-6">
+    <img  src="./src/assets/profile-pic.jpg" className="profile-pic float-end imgshadow me-3" alt="profile-pic"/>
+    <h1 className="display-1"><u>About.</u></h1>
+    <figure>
+  <blockquote className="blockquote">
+    <p><small>Welcome to my portfolio site! My name is Paul Amago. I came from a small town in Minnesota called Baudette.
+        I love all forms of art specifically painting and drawing as they are ways that we can express ourselves. 
+        I aspire to help businesses express themselves thorugh their websites.</small> </p>
+  </blockquote>
+</figure>
+    </div>
+  </div>
+</div>
+   
+    </>)
+
+    
+}
+
+export default AboutMe;
